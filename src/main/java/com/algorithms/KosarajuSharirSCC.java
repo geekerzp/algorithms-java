5eab4b2ef31128d519adb06843cf16b7b7159946
geekerzp <geekerzp@gmail.com>
2017-05-27 22:19:30 +0800

    Add KosarajuSharirSCC

---
+++ src/main/java/com/algorithms/KosarajuSharirSCC.java
@@ -0,0 +1,39 @@
+package com.algorithms;
+
+public class KosarajuSharirSCC {
+  private boolean[] marked; // reached vertices
+  private int[] id; // component identifiers
+  private int count; // number of strong components
+
+  public KosarajuSharirSCC(Digraph G) {
+    marked = new boolean[G.V()];
+    id = new int[G.V()];
+    DepthFirstOrder order = new DepthFirstOrder(G.reverse());
+    for (int s : order.reversePost()) {
+      if (!marked[s]) {
+        dfs(G, s);
+        count++;
+      }
+    }
+  }
+
+  private void dfs(Digraph G, int v) {
+    marked[v] = true;
+    id[v] = count;
+    for (int w : G.adj(v)) {
+      if (!marked[w]) dfs(G, w);
+    }
+  }
+
+  public boolean stronglyConnected(int v, int w) {
+    return id[v] == id[w];
+  }
+
+  public int id(int v) {
+    return id[v];
+  }
+
+  public int count() {
+    return count;
+  }
+}
