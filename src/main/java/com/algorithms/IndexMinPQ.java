995fd29c425fa0154470ae9a4851805d1f295263
geekerzp <geekerzp@gmail.com>
2017-06-30 00:07:01 +0800

    Update IndexMinPQ

--- src/main/java/com/algorithms/IndexMinPQ.java
+++ src/main/java/com/algorithms/IndexMinPQ.java
@@ -184,5 +184,74 @@ public class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer
     if (!contains(i)) {
       throw new NoSuchElementException("index is not in the priority queue");
     }
+    if (keys[i].compareTo(key) <= 0) {
+      throw new IllegalArgumentException(
+          "Calling decreaseKey() with given argument would not strictly decrease the key");
+    }
+    keys[i] = key;
+    swim(qp[i]);
+  }
+
+  /** Increase the key associated with index {@code i} to the specified value. */
+  public void increaseKey(int i, Key key) {
+    if (i < 0 || i >= maxN) {
+      throw new IndexOutOfBoundsException();
+    }
+    if (!contains(i)) {
+      throw new NoSuchElementException("index is not in the priority queue");
+    }
+    if (keys[i].compareTo(key) >= 0) {
+      throw new IllegalArgumentException(
+          "Calling increaseKey() with given argument would not strictly increase the key");
+    }
+    keys[i] = key;
+    sink(qp[i]);
+  }
+
+  /** Remove the key associated with index {@code i}. */
+  public void delete(int i) {
+    if (i < 0 || i >= maxN) {
+      throw new IndexOutOfBoundsException();
+    }
+    if (!contains(i)) {
+      throw new NoSuchElementException("index is not in the priority queue");
+    }
+    int index = qp[i];
+    exch(index, n--);
+    swim(index);
+    sink(index);
+    keys[i] = null;
+    qp[i] = -1;
+  }
+
+  /** General helper functions. */
+  private boolean greater(int i, int j) {
+    return keys[pq[i]].compareTo(keys[pq[j]]) > 0;
+  }
+
+  private void exch(int i, int j) {
+    int swap = pq[i];
+    pq[i] = pq[j];
+    pq[j] = swap;
+    qp[pq[i]] = i;
+    qp[pq[j]] = j;
+  }
+
+  /** Heap helper functions. */
+  private void swim(int k) {
+    while (k > 1 && greater(k / 2, k)) {
+      exch(k, k / 2);
+      k = k / 2;
+    }
+  }
+
+  private void sink(int k) {
+    while (2 * k <= n) {
+      int j = 2 * k;
+      if (j < n && greater(j, j + 1)) j++;
+      if (!greater(k, j)) break;
+      exch(k, j);
+      k = j;
+    }
   }
 }
